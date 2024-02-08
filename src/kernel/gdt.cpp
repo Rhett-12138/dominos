1dee568da768821c7ea8de3de74ab34f54da048c
rhettLi <moying20@foxmail.com>
2024-02-08 20:28:12 +0800

    fix: correctly load gdt

--- src/kernel/gdt.cpp
+++ src/kernel/gdt.cpp
@@ -19,51 +19,60 @@ extern "C" void gdt_init()
 {
     LOG("init gdt!!!");
 
-    memset(gdt, 0, sizeof(gdt));
-
-    descriptor_t *desc;
-    desc = gdt + KERNEL_CODE_IDX;
-    descriptor_init(desc, 0, 0xFFFFF);
-    desc->segment = 1;     // 代码段
-    desc->granularity = 1; // 4K
-    desc->big = 1;         // 32 位
-    desc->long_mode = 0;   // 不是 64 位
-    desc->present = 1;     // 在内存中
-    desc->DPL = 0;         // 内核特权级
-    desc->type = 0b1010;   // 代码 / 非依从 / 可读 / 没有被访问过
-
-    desc = gdt + KERNEL_DATA_IDX;
-    descriptor_init(desc, 0, 0xFFFFF);
-    desc->segment = 1;     // 数据段
-    desc->granularity = 1; // 4K
-    desc->big = 1;         // 32 位
-    desc->long_mode = 0;   // 不是 64 位
-    desc->present = 1;     // 在内存中
-    desc->DPL = 0;         // 内核特权级
-    desc->type = 0b0010;   // 数据 / 向上增长 / 可写 / 没有被访问过
-
-    desc = gdt + USER_CODE_IDX;
-    descriptor_init(desc, 0, 0xFFFFF);
-    desc->segment = 1;     // 代码段
-    desc->granularity = 1; // 4K
-    desc->big = 1;         // 32 位
-    desc->long_mode = 0;   // 不是 64 位
-    desc->present = 1;     // 在内存中
-    desc->DPL = 3;         // 用户特权级
-    desc->type = 0b1010;   // 代码 / 非依从 / 可读 / 没有被访问过
-
-    desc = gdt + USER_DATA_IDX;
-    descriptor_init(desc, 0, 0xFFFFF);
-    desc->segment = 1;     // 数据段
-    desc->granularity = 1; // 4K
-    desc->big = 1;         // 32 位
-    desc->long_mode = 0;   // 不是 64 位
-    desc->present = 1;     // 在内存中
-    desc->DPL = 3;         // 用户特权级
-    desc->type = 0b0010;   // 数据 / 向上增长 / 可写 / 没有被访问过
+    asm volatile("sgdt gdt_ptr\n");
+    memcpy(&gdt, (void*)gdt_ptr.base, gdt_ptr.limit+1);
 
     gdt_ptr.base = (uint32_t)&gdt;
-    gdt_ptr.limit = sizeof(gdt) - 1;
+    gdt_ptr.limit = sizeof(gdt)-1;
+
+    asm volatile("lgdt gdt_ptr\n");
+
+
+    // memset(gdt, 0, sizeof(gdt));
+
+    // descriptor_t *desc;
+    // desc = gdt + KERNEL_CODE_IDX;
+    // descriptor_init(desc, 0, 0xFFFFF);
+    // desc->segment = 1;     // 代码段
+    // desc->granularity = 1; // 4K
+    // desc->big = 1;         // 32 位
+    // desc->long_mode = 0;   // 不是 64 位
+    // desc->present = 1;     // 在内存中
+    // desc->DPL = 0;         // 内核特权级
+    // desc->type = 0b1010;   // 代码 / 非依从 / 可读 / 没有被访问过
+
+    // desc = gdt + KERNEL_DATA_IDX;
+    // descriptor_init(desc, 0, 0xFFFFF);
+    // desc->segment = 1;     // 数据段
+    // desc->granularity = 1; // 4K
+    // desc->big = 1;         // 32 位
+    // desc->long_mode = 0;   // 不是 64 位
+    // desc->present = 1;     // 在内存中
+    // desc->DPL = 0;         // 内核特权级
+    // desc->type = 0b0010;   // 数据 / 向上增长 / 可写 / 没有被访问过
+
+    // desc = gdt + USER_CODE_IDX;
+    // descriptor_init(desc, 0, 0xFFFFF);
+    // desc->segment = 1;     // 代码段
+    // desc->granularity = 1; // 4K
+    // desc->big = 1;         // 32 位
+    // desc->long_mode = 0;   // 不是 64 位
+    // desc->present = 1;     // 在内存中
+    // desc->DPL = 3;         // 用户特权级
+    // desc->type = 0b1010;   // 代码 / 非依从 / 可读 / 没有被访问过
+
+    // desc = gdt + USER_DATA_IDX;
+    // descriptor_init(desc, 0, 0xFFFFF);
+    // desc->segment = 1;     // 数据段
+    // desc->granularity = 1; // 4K
+    // desc->big = 1;         // 32 位
+    // desc->long_mode = 0;   // 不是 64 位
+    // desc->present = 1;     // 在内存中
+    // desc->DPL = 3;         // 用户特权级
+    // desc->type = 0b0010;   // 数据 / 向上增长 / 可写 / 没有被访问过
+
+    // gdt_ptr.base = (uint32_t)&gdt;
+    // gdt_ptr.limit = sizeof(gdt) - 1;
 }
 
 void tss_init()
